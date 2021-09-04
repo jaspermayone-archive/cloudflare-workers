c011f2c5153a2c9cfd9141c852baae30babcf90a
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-09-04 13:48:03 -0700

    feat: update deps

--- src/database/connectDatabase.ts
+++ src/database/connectDatabase.ts
@@ -5,9 +5,6 @@ import { logHandler } from "../utils/logHandler";
  * This function initialises the database connection.
  */
 export const connectDatabase = async (): Promise<void> => {
-  await connect(process.env.MONGO_URI || "", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  await connect(process.env.MONGO_URI || "");
   logHandler.log("debug", "connected to database!");
 };
