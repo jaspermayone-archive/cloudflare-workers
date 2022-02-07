c309c72de366aa56d5a066b947e9a224fbd70979
Jasper Mayone <j.dogcoder@gmail.com>
2022-02-07 09:30:32 -0500

    Update links-worker.js

--- links-worker.js
+++ links-worker.js
@@ -2,6 +2,7 @@ const redirectMap = new Map([
   ["discord", "https://discord.gg/PD3kuBzZ6p"],
   ["github", "https://github.com/j-dogcoder"],
   ["instagram", "https://instagram.com/jasper_mayone"],
+  ["photo-instagram", "https://instagram.com/jasper.mayone.photography"],
   ["linkedin", "https://www.linkedin.com/in/jasper-mayone-70033b164/"],
   ["reddit", "https://www.reddit.com/user/j-dogcoder"],
   ["stackoverflow", "https://stackoverflow.com/users/15201146/j-dogcoder"],
